c743f102ffe8963d365c00eeb1749b129ad04e54
sestinj <33237525+sestinj@users.noreply.github.com>
2020-10-19 23:04:53 -0400

    more changes to mtc.py

--- mtc.py
+++ mtc.py
@@ -3,7 +3,25 @@ from zipfile import ZipFile
 import re
 import nltk
 
+ROOT_DIR = '/Users/natesesti/Desktop/mtc'
 
+def convert_to_bits(string):
+    """Create and return an array of bits for each character of a string"""
+
+    bytes = bytearray(string, 'utf-8')
+    bitarray = list(map(bin, bytes))
+    return bitarray
+
+def create_conversion(n=8, path=ROOT_DIR + '/masc_500k_texts'):
+    """Create a dictionary of size 2^n (default can be represented by a single byte)
+    that maps most common tokens to binary.
+    Uses the masc_500k_texts corpus by default"""
+
+    common = most_common(tokenize_directory(path), 2**n)
+    conversion = {}
+    for i in range(len(common)):
+        conversion[common[i][0]] = bin(i).split('0b')[1]
+    return conversion
 
 def most_common(tokens, n=50):
     return nltk.FreqDist(tokens).most_common(n)
@@ -31,16 +49,19 @@ def tokenize_directory(path=os.getcwd()):
 
     return tokens
 
-def create_map(tokens):
-    pass
-
-def compress(path, map):
+def compress(path, conversion):
     """Given path to a file,
     create a new compressed .txt file, named path+_compressed.txt.
     Return the path to the new file."""
+    os.chdir(ROOT_DIR)
+    write_file = open(path.split('.')[0] + '_compressed.txt', 'wb')
 
-    file = open(path.split('.')[0] + '_compressed.txt', 'w')
-    file.write()
+    for token in tokenize_file(path):
+        if token in conversion:
+            write_file.write(conversion[token])
+        else:
+            for bits in convert_to_bits(token):
+                write_file.write(bits)
 
     return path
 
